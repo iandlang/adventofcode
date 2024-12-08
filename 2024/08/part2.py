e62843bf32a76c2fab12bc96291a91a520a69439
Ian Lang <ian.d.lang@gmail.com>
2024-12-08 08:52:17 +0000

    2024 Day 8 Part 1 & 2

---
+++ 2024/08/part2.py
@@ -0,0 +1,48 @@
+from itertools import count
+import numpy as np
+import string
+import sys
+from decorators.timer import timer
+
+
+def get_data(file:str) -> np.ndarray[str]:
+
+    return np.genfromtxt(file, converters={0:list})
+
+
+def compute(data:np.ndarray[str]) -> None:
+
+    rowcount, colcount = data.shape
+    antinodes = np.zeros(data.shape, dtype=bool)
+
+    for c in string.ascii_lowercase + string.ascii_uppercase + "0123456789":
+
+        cells = np.argwhere(data == c)
+
+        for c1 in cells:
+            for c2 in cells:
+                if not np.array_equal(c1,c2):
+                    for i in count(0):
+                        c = c1 + (c1 - c2) * i
+                        if 0 <= c[0] < rowcount and 0 <= c[1] < colcount:
+                            antinodes[c[0],c[1]] = True
+                        else:
+                            break
+
+    result = np.count_nonzero(antinodes)
+    print(result)
+
+    answer = 1045
+    if answer:
+        assert(result == answer)
+
+
+@timer
+def main() -> None:
+
+    data = get_data(sys.argv[1])
+    compute(data)
+
+
+if __name__ == "__main__":
+    main()
