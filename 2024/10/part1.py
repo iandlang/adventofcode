1afec357e782c9c64430b01c1649f4c4346cd7fa
Ian Lang <ian.d.lang@gmail.com>
2024-12-10 06:56:03 +0000

    2024 Day 10 Part 1 & 2

---
+++ 2024/10/part1.py
@@ -0,0 +1,60 @@
+import numpy as np
+import sys
+from decorators.timer import timer
+
+
+def get_data(file:str) -> np.ndarray[str]:
+
+    return np.genfromtxt(file, comments=None, converters={0:list})
+
+
+def compute(data:np.ndarray[str]) -> None:
+
+    def solve(grid, loc):
+
+        r,c = loc
+        height = int(grid[r,c])
+
+        if height == 9 and not visited[r,c]:
+            visited[r,c] = True
+            paths[thr,thc] += 1
+            return
+
+        if r > 0 and int(grid[r-1,c]) == height + 1:
+                solve(grid, [r-1,c])
+
+        if c < n-1 and int(grid[r,c+1]) == height + 1:
+                solve(grid, [r,c+1])
+
+        if r < n-1 and int(grid[r+1,c]) == height + 1:
+                solve(grid, [r+1,c])
+
+        if c > 0 and int(grid[r,c-1]) == height + 1:
+                solve(grid, [r,c-1])
+
+    n = len(data)
+    trailheads = np.argwhere(data == '0')
+    paths=np.zeros(shape=data.shape, dtype=int)
+
+    for th in trailheads:
+        visited=np.zeros(data.shape, dtype=bool)
+        thr,thc = th
+        solve(data,th)
+
+    result = np.sum(paths)
+    print(result)
+
+    answer = 822
+    if answer:
+        assert(result == answer)
+
+
+@timer
+def main() -> None:
+
+    data = get_data(sys.argv[1])
+    compute(data)
+
+
+if __name__ == "__main__":
+    main()
