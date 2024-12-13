9e7b7de6b8269445fae60ad5f3933d2633c9f7e5
Ian Lang <ian.d.lang@gmail.com>
2024-12-13 10:53:05 +0000

    2024 Day 13 Part 1 & 2

---
+++ 2024/13/part1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import re
+import sys
+from decorators.timer import timer
+from typing import List
+
+def get_data(file:str) -> List[List[int]]:
+
+    with open(file) as f:
+        return [list(map(int,re.findall(r'\d+',line))) for line in f.read().split("\n\n")]
+
+
+def compute(machines:List[List[int]]) -> None:
+
+    result = 0
+
+    for ax,ay,bx,by,px,py in machines:
+
+        '''
+        a(ax) + b(bx) = px
+        a(ay) + b(by) = py
+
+        solve for a and b, and check both are integers
+        '''
+
+        a = (px*by-py*bx)/(ax*by-bx*ay)
+        b = (px*ay-py*ax)/(bx*ay-ax*by)
+
+        if a.is_integer() and b.is_integer():
+            result += int(3*a + b)
+
+    print(result)
+
+    answer = 30413
+    if answer:
+        assert(result == answer)
+
+
+@timer
+def main() -> None:
+
+    data = get_data(sys.argv[1])
+    compute(data)
+
+
+if __name__ == "__main__":
+    main()
