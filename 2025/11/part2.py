5fc48b3ee8720b4218aaaaac6f5f760c9ce431b7
Ian Lang <ian.lang@eonnext.com>
2025-12-19 16:43:22 +0000

    2025 day 11 part2 tidyup

--- 2025/11/part2.py
+++ 2025/11/part2.py
@@ -1,35 +1,30 @@
 from decorators.timer import timer
-from functools import lru_cache, reduce
-from operator import mul
+from functools import cache
+from math import prod
 from itertools import pairwise
 
-def load_data(file: str):
+Node = str
+Network = dict[Node, list[Node]]
 
+def load_data(file: str) -> Network:
     with open(file) as f:
-        return {
-            line[0][:-1]: line[1:]
-            for line in [line.split(" ") for line in f.read().splitlines()]
-        }
+        return {nodes[0][:-1]: nodes[1:] for nodes in [line.split(" ") for line in f.read().splitlines()]}
 
-def solve(network: dict[str, list[str]]) -> int:
+def solve(network: Network) -> int:
 
-    @lru_cache
-    def path_count(node_from: str, node_to: str) -> int:
+    @cache
+    def path_count(node_from: Node, node_to: Node) -> int:
         if node_from == node_to:
-            pass
             return 1
-
         if node_from not in network:
-            pass
             return 0
+        return sum(path_count(next_node, node_to) for next_node in network[node_from])
 
-        next_nodes = network[node_from]
-        return sum(path_count(next_node, node_to) for next_node in next_nodes)
-
-    return sum([
-        reduce(mul,(path_count(f,t) for f,t in pairwise(["svr", "fft", "dac", "out"]))),
-        reduce(mul,(path_count(f,t) for f,t in pairwise(["svr", "dac", "fft", "out"])))
-    ])
+    paths = [
+        ["svr", "fft", "dac", "out"],
+        ["svr", "dac", "fft", "out"]
+    ]
+    return sum(prod(path_count(node_from, node_to) for node_from, node_to in pairwise(path)) for path in paths)
 
 @timer
 def main() -> None:
